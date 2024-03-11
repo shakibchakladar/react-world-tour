c2b6e9eb229e89bf2fff235afabfedd9c3dc2dee
shakibchakladar <shakibchakladar@gmail.com>
2024-03-12 00:22:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './component/countrys/countries'
+
+function App() {
+
+  return (
+    <>
+      <h1> React world tour</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/country/country.jsx
@@ -0,0 +1,14 @@
+import './country .css'
+const Country = ({country}) => {
+    console.log(country)
+    const {name,flags}=country
+    return (
+        <div className="country">
+            <h3>name:{name?.common}</h3>
+            <img src= {flags.png} alt="" />
+            
+        </div>
+    );
+};
+
+export default Country;
